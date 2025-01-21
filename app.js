8851021cbb8fc3989f29125c965ac1c104c8b2fd
Feben-A <afa508165@gmail.com>
2025-01-21 09:54:53 +0000

    Add cors to project

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 // console.log("Hello")
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const logger = require("./logger");
 
 //another middleware
-
+app.use(cors());
 app.use(express.json());
 app.use(logger);
-//we dont want 
+//we dont want
 
 const fruitsRouter = require("./routes/fruits.js");
-
 
 // const fruits = require("./fruits.json");
 
